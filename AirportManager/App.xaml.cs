08ee1f0e96ea982324e9251435999aba1091757f
DESKTOP-97DR3E3\poijn <mykhailo.ishchenko.work@gmail.com>
2024-03-27 21:08:00 +0200

    Add project files.

---
+++ AirportManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AirportManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
